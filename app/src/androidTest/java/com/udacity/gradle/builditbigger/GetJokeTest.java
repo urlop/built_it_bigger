78782e6670c5134f312bf56a6e21c7e36d0c40ff
usagiChan <ruby_93lop40_21@hotmail.com>
2016-02-01 23:35:01 -0500

    Add code references

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/GetJokeTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/GetJokeTest.java
@@ -3,6 +3,7 @@ package com.udacity.gradle.builditbigger;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
+//BASED ON: https://github.com/mmazzarolo/build-it-bigger/blob/master/app/src/androidTest/java/com/udacity/gradle/builditbigger/NonEmptyStringTest.java
 public class GetJokeTest extends AndroidTestCase {
 
     private static final String LOG_TAG = "GetJokeTest";
